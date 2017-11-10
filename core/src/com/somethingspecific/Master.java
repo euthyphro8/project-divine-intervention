2a5279e56d681b60c4701d526b4d6a705a3697ea
Josh Hess <joshhess13@gmail.com>
2017-11-10 09:46:03 -0700

    Project init

--- core/src/com/somethingspecific/Master.java
+++ core/src/com/somethingspecific/Master.java
@@ -1,27 +1,50 @@
 package com.somethingspecific;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.somethingspecific.framework.State;
+import com.somethingspecific.framework.ScreenManager;
+import com.somethingspecific.framework.GameManager;
+import com.somethingspecific.framework.LoadManager;
+import com.somethingspecific.framework.MenuManager;
+import com.somethingspecific.graphics.SpriteSheet;
 
 public class Master extends ApplicationAdapter {
 
 
+
+
+
 	public static State state;
+
+	public GameManager game;
+	public ScreenManager screen;
+	public LoadManager load;
+	public MenuManager menu;
+
+
 
 	@Override
 	public void create () {
-		state = State.LOADING;
+		state = State.GAME;
+		screen = new ScreenManager();
+		game = new GameManager(load);
+		load = new LoadManager();
+		menu = new MenuManager();
+
+		SpriteSheet.init();
 	}
 
 
 
 	public void update() {
 		if(state == State.LOADING) {
-
+			load.update();
 		}else if(state == State.MENU) {
-
+			menu.update();
 		}else if(state == State.GAME) {
-
+			game.update();
 		}else if(state == State.EXITING) {
 
 		}
@@ -32,6 +55,10 @@ public class Master extends ApplicationAdapter {
 		update();
 
 
+
+		screen.start();
+		screen.render(SpriteSheet.bigHead, 150, 150);
+
 		if(state == State.LOADING) {
 
 		}else if(state == State.MENU) {
@@ -41,8 +68,7 @@ public class Master extends ApplicationAdapter {
 		}else if(state == State.EXITING) {
 
 		}
-
-
+		screen.stop();
 
 	}
 	
---
+++ core/src/com/somethingspecific/framework/MenuManager.java
@@ -0,0 +1,16 @@
+package com.somethingspecific.framework;
+
+import java.awt.*;
+
+public class MenuManager {
+
+    public MenuManager() {
+
+    }
+
+    public void update() {
+
+    }
+    public void render(ScreenManager screen) {
+    }
+}
